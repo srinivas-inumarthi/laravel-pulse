0463ce119f6ece4e2ba76c316678f18e2ba9e9d5
srinivas-inumarthi <srinivas.inumarthi@goapptiv.com>
2025-12-11 15:17:48 +0530

    Update status to processed

--- src/Listeners/SendSlowRequestNotification.php
+++ src/Listeners/SendSlowRequestNotification.php
@@ -2,16 +2,19 @@
 
 namespace Goapptiv\Pulse\Listeners;
 
+use Goapptiv\Pulse\Constants;
 use Goapptiv\Pulse\Events\SlowRequest;
-use Illuminate\Support\Facades\DB;
+use Goapptiv\Pulse\Repositories\PulseEntryRepositoryInterface;
+use Illuminate\Support\Facades\Log;
 
 class SendSlowRequestNotification
 {
     /**
      * Create the event listener.
      */
-    public function __construct()
-    {
+    public function __construct(
+        private PulseEntryRepositoryInterface $pulseEntryRepository
+    ) {
         //
     }
 
@@ -20,7 +23,10 @@ class SendSlowRequestNotification
      */
     public function handle(SlowRequest $event): void
     {
-       $request = DB::table('pulse_entries')->find($event->id);
-       dd($request);
+        Log::info("Sending slow request notification by :",['id' => $event->id]);
+        $request = $this->pulseEntryRepository->findById($event->id);
+
+        Log::info("Updating status to processed by :",['id' => $request->id]);
+        $this->pulseEntryRepository->updateStatusById($request->id, Constants::$PROCESSED);
     }
 }
